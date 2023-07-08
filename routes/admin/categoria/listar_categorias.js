ab307da67c775533f549e54e404446f8145cf66c
DavidTMaciel <davidmacieljr122@gmail.com>
2023-07-08 17:57:04 -0300

    rota listar categorias

---
+++ routes/admin/categoria/listar_categorias.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+require('../../../models/categorias')
+const Categoria = mongoose.model('Categoria');
+
+router.get('/', (req, res) => {
+    res.render("../views/admin/categoria/admin");
+});
+
+router.get('/categorias', (req, res) => {
+    Categoria.find().lean().then((categorias)=>{
+        res.render("admin/categoria", {categorias: categorias})
+    }).catch((err) => {
+        req.flash('error', "Houve um erro ao registar a categoria");
+        res.redirect("/admin")
+    });
+});
+
+module.exports = router;
